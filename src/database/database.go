5c50a3aacc7939bfb250f926ea0b58dba2b12db1
Alex Bates <alexlbates@gmail.com>
2022-08-29 10:21:14 -0400

    set foreign key support for db

--- src/database/database.go
+++ src/database/database.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"log"
 )
 
 func Connect(connectionString string) *gorm.DB {
 	db, err := gorm.Open(sqlite.Open(connectionString), &gorm.Config{})
 	if err != nil {
 		panic(fmt.Errorf("unable to connect to database: %w", err))
+	}
+
+	if err = db.Exec("PRAGMA foreign_keys = ON", nil).Error; err != nil {
+		log.Fatalf("could not enable foreign key support. failing outright.")
 	}
 
 	return db
