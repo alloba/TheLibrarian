f60f0582e373e6bbaa3b52f71377401f0c6d16b2
Alex Bates <alexlbates@gmail.com>
2022-08-29 09:03:53 -0400

    convert repo and service structs to use receivers instead of method fields

--- src/database/recordrepo.go
+++ src/database/recordrepo.go
@@ -7,52 +7,36 @@ import (
 
 func NewRecordRepo(db *gorm.DB) *RecordRepo {
 	return &RecordRepo{
-		db:         db,
-		FindAll:    getAllRecords(db),
-		SaveOne:    saveSingleRecord(db),
-		FindByHash: findSingleRecordByHash(db),
+		db: db,
 	}
 }
 
 type RecordRepo struct {
-	db         *gorm.DB
-	FindAll    findAllRecords
-	SaveOne    saveRecord
-	FindByHash findRecordByHash
+	db *gorm.DB
 }
 
-type findAllRecords func() ([]Record, error)
-type saveRecord func(record *Record) error
-type findRecordByHash func(hash string) (*Record, error)
-
-func getAllRecords(db *gorm.DB) func() ([]Record, error) {
-	return func() ([]Record, error) {
-		var records []Record
-		result := db.Find(&records)
-		if result.Error != nil {
-			return nil, fmt.Errorf("could not get all records from database - %v", result.Error.Error())
-		}
-		return records, nil
+func (repo *RecordRepo) FindAll() ([]Record, error) {
+	var records []Record
+	result := repo.db.Find(&records)
+	if result.Error != nil {
+		return nil, fmt.Errorf("could not get all records from database - %v", result.Error.Error())
 	}
+	return records, nil
 }
 
-func findSingleRecordByHash(db *gorm.DB) func(hash string) (*Record, error) {
-	return func(hash string) (*Record, error) {
-		var record Record
-		result := db.Where("hash = ?", hash).First(&record)
-		if result.Error != nil {
-			return nil, fmt.Errorf("failed to find record - %v", result.Error.Error())
-		}
-		return &record, nil
+func (repo *RecordRepo) FindByHash(hash string) (*Record, error) {
+	var record Record
+	result := repo.db.Where("hash = ?", hash).First(&record)
+	if result.Error != nil {
+		return nil, fmt.Errorf("failed to find record - %v", result.Error.Error())
 	}
+	return &record, nil
 }
 
-func saveSingleRecord(db *gorm.DB) func(record *Record) error {
-	return func(record *Record) error {
-		result := db.Create(record)
-		if result.Error != nil {
-			return fmt.Errorf("could not save record to database - %v", result.Error.Error())
-		}
-		return nil
+func (repo *RecordRepo) SaveOne(record *Record) error {
+	result := repo.db.Create(record)
+	if result.Error != nil {
+		return fmt.Errorf("could not save record to database - %v", result.Error.Error())
 	}
+	return nil
 }
--- src/service/recordservice.go
+++ src/service/recordservice.go
@@ -12,6 +12,11 @@ import (
 	"time"
 )
 
+type RecordService struct {
+	recordRepo *database.RecordRepo
+	basePath   string
+}
+
 func New(repo *database.RecordRepo, archiveBasePath string) *RecordService {
 	absPath, err := filepath.Abs(archiveBasePath)
 	if err != nil {
@@ -21,107 +26,89 @@ func New(repo *database.RecordRepo, archiveBasePath string) *RecordService {
 	if err != nil || !stat.IsDir() {
 		log.Fatalf("either couldnt read file stats for absPath or it isn't a directory.")
 	}
-	basePath = absPath + string(os.PathSeparator)
 
 	return &RecordService{
-		recordRepo:            repo,
-		CreateRecordData:      createRecordFromFile(),
-		PersistRecordIfUnique: persistRecordIfNotAlreadyExists(repo),
+		recordRepo: repo,
+		basePath:   absPath + string(os.PathSeparator),
 	}
 }
 
-type RecordService struct {
-	recordRepo            *database.RecordRepo
-	CreateRecordData      processFile
-	PersistRecordIfUnique persistRecord
+func (service RecordService) CreateRecordData(filepath string) (*database.Record, error) {
+	file, err := fileutil.GetFileBinary(filepath)
+	if err != nil {
+		return nil, fmt.Errorf("unable to get file binary - %v", err.Error())
+	}
+	defer file.Close()
+
+	fileHash, err := fileutil.GetFileHash(file)
+	if err != nil {
+		return nil, fmt.Errorf("unable to get file hash - %v", err.Error())
+	}
+
+	qualifiedPath, err := fileutil.GetQualifiedFilePath(filepath)
+	if err != nil {
+		return nil, fmt.Errorf("could not get full path of file - %v", err.Error())
+	}
+
+	stat, err := file.Stat()
+	if err != nil {
+		return nil, fmt.Errorf("could not load file stats - %v", err.Error())
+	}
+
+	var pathBits = strings.Split(qualifiedPath, string(os.PathSeparator))
+	var fname = pathBits[len(pathBits)-1]
+	var ext = "." + strings.Split(fname, ".")[len(strings.Split(fname, "."))-1]
+
+	var recordObj = &database.Record{
+		Hash:             fileHash,
+		FilePointer:      service.basePath + generateUniqueSubPath(fileHash) + ".bin",
+		Name:             fname,
+		Extension:        ext,
+		DateFileModified: stat.ModTime(),
+		DateCreated:      time.Now(),
+		DateModified:     time.Now(),
+	}
+
+	return recordObj, nil
 }
 
-type processFile func(filepath string) (*database.Record, error)
-type persistRecord func(record *database.Record, path string) (bool, error)
-
-//local persist. cross fingers for assumed singleton behavior never screwing up O_O
-var basePath string
-
-func createRecordFromFile() func(filepath string) (*database.Record, error) {
-	return func(filepath string) (*database.Record, error) {
-		file, err := fileutil.GetFileBinary(filepath)
-		if err != nil {
-			return nil, fmt.Errorf("unable to get file binary - %v", err.Error())
-		}
-		defer file.Close()
-
-		fileHash, err := fileutil.GetFileHash(file)
-		if err != nil {
-			return nil, fmt.Errorf("unable to get file hash - %v", err.Error())
-		}
-
-		qualifiedPath, err := fileutil.GetQualifiedFilePath(filepath)
-		if err != nil {
-			return nil, fmt.Errorf("could not get full path of file - %v", err.Error())
-		}
-
-		stat, err := file.Stat()
-		if err != nil {
-			return nil, fmt.Errorf("could not load file stats - %v", err.Error())
-		}
-
-		var pathBits = strings.Split(qualifiedPath, string(os.PathSeparator))
-		var fname = pathBits[len(pathBits)-1]
-		var ext = "." + strings.Split(fname, ".")[len(strings.Split(fname, "."))-1]
-
-		var recordObj = &database.Record{
-			Hash:             fileHash,
-			FilePointer:      basePath + generateUniqueSubPath(fileHash) + ".bin",
-			Name:             fname,
-			Extension:        ext,
-			DateFileModified: stat.ModTime(),
-			DateCreated:      time.Now(),
-			DateModified:     time.Now(),
-		}
-
-		return recordObj, nil
+func (service RecordService) PersistRecordIfUnique(record *database.Record, originPath string) (bool, error) {
+	_, err := service.recordRepo.FindByHash(record.Hash)
+	if err == nil {
+		//no record found means no new record needed.
+		return false, nil
 	}
-}
 
-func persistRecordIfNotAlreadyExists(repo *database.RecordRepo) func(record *database.Record, originPath string) (bool, error) {
-	return func(record *database.Record, originPath string) (bool, error) {
-		_, err := repo.FindByHash(record.Hash)
-		if err == nil {
-			//no record found means no new record needed.
-			return false, nil
-		}
-
-		file, err := fileutil.GetFileBinary(originPath)
-		if err != nil {
-			return false, fmt.Errorf("unable to get file binary - %v", err.Error())
-		}
-		defer file.Close()
-
-		fileDestination, err := os.Create(record.FilePointer)
-		if err != nil {
-			return false, fmt.Errorf("unable to access record filepointer - %v", err.Error())
-		}
-		defer fileDestination.Close()
-
-		_, err = io.Copy(fileDestination, file)
-		if err != nil {
-			return false, fmt.Errorf("unable to write bytes to record filepointer - %v", err.Error())
-		}
-
-		err = repo.SaveOne(record)
-		if err != nil {
-			quietAttemptDelete(record.FilePointer)
-			return false, fmt.Errorf("unable to save new record to database - %v", err.Error())
-		}
-
-		_, err = repo.FindByHash(record.Hash)
-		if err != nil {
-			quietAttemptDelete(record.FilePointer)
-			return false, fmt.Errorf("could not ack record in database: %v", record.Hash)
-		}
-
-		return true, nil
+	file, err := fileutil.GetFileBinary(originPath)
+	if err != nil {
+		return false, fmt.Errorf("unable to get file binary - %v", err.Error())
 	}
+	defer file.Close()
+
+	fileDestination, err := os.Create(record.FilePointer)
+	if err != nil {
+		return false, fmt.Errorf("unable to access record filepointer - %v", err.Error())
+	}
+	defer fileDestination.Close()
+
+	_, err = io.Copy(fileDestination, file)
+	if err != nil {
+		return false, fmt.Errorf("unable to write bytes to record filepointer - %v", err.Error())
+	}
+
+	err = service.recordRepo.SaveOne(record)
+	if err != nil {
+		quietAttemptDelete(record.FilePointer)
+		return false, fmt.Errorf("unable to save new record to database - %v", err.Error())
+	}
+
+	_, err = service.recordRepo.FindByHash(record.Hash)
+	if err != nil {
+		quietAttemptDelete(record.FilePointer)
+		return false, fmt.Errorf("could not ack record in database: %v", record.Hash)
+	}
+
+	return true, nil
 }
 
 func generateUniqueSubPath(hash string) string {
