55a16787ca8bce05a2b379b4f95537c68c06ca25
Alex Bates <alexlbates@gmail.com>
2022-09-11 11:14:23 -0400

    todo about repo collector

--- src/database/repository/collector.go
+++ src/database/repository/collector.go
@@ -3,7 +3,9 @@ package repository
 import (
 	"gorm.io/gorm"
 )
-
+//TODO: This honestly feels like a progressively worse idea the longer i use it.
+//      Either i should fall into full dependency injection, 
+//      or i should explicitly insert pointers in services that need them...
 type RepoManager struct {
 	Record  *RecordRepo
 	Book    *BookRepo
